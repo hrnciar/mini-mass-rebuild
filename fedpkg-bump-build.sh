6ed5e959c87d365bce602c2ecd7490372fec523d
Miro Hrončok <miro@hroncok.cz>
2021-05-31 15:33:05 +0200

    Prepare fedpkg-build.sh for Python 3.10 rebuild

--- fedpkg-bump-build.sh
+++ fedpkg-bump-build.sh
@@ -2,12 +2,12 @@ set -o pipefail  # make tee preserve the exit code
 fedpkg clone $1  2>&1 | tee ./${1}.log || exit $?
 
 cd $1
-  if ! git show --name-only | grep -F "Python 3.9"; then
-    rpmdev-bumpspec -c "Rebuilt for Python 3.9" *.spec | tee -a ../${1}.log
-    git commit *.spec -m "Rebuilt for Python 3.9" | tee -a ../${1}.log
+  if ! git show --name-only | grep -F "Python 3.10"; then
+    rpmdev-bumpspec -c "Rebuilt for Python 3.10" --userstring="Python Maint <python-maint@redhat.com>" *.spec | tee -a ../${1}.log
+    git commit *.spec -m "Rebuilt for Python 3.10" --author="Python Maint <python-maint@redhat.com>" | tee -a ../${1}.log
     git push
   fi
-  fedpkg build --fail-fast --nowait --background 2>&1 | tee -a ../${1}.log
+  fedpkg build --target=f35-python --fail-fast --nowait --background 2>&1 | tee -a ../${1}.log
 cd -
 
 rm -rf $1
